c07917b61ed2c2ae2561ae599ea771babeb7aa84
jojazpeitia <jojazpeitia@gmail.com>
2023-12-07 20:58:20 -0600

    fixed random component values not working on prod build

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DialogBox from '@/components/ui/DialogBox';
-
 
 export default function Home() {
   const [isDialogOpen, setDialogOpen] = useState(true);
+  const [hasRandomPosition, setHasRandomPosition] = useState(false);
+  const [dialogPosition, setDialogPosition] = useState(getRandomPosition());
+
+  useEffect(() => {
+    // Update random position values on page reload
+    setDialogPosition(getRandomPosition());
+    setHasRandomPosition(true);
+  }, []); // Empty dependency array ensures this effect runs only once on mount
 
   const handleOpenDialog = () => {
     setDialogOpen(true);
@@ -14,38 +21,40 @@ export default function Home() {
     setDialogOpen(false);
   };
 
-  const getRandomPosition = () => ({
-    x: Math.floor(Math.random() * 800), // Adjust the values as needed
-    y: Math.floor(Math.random() * 600), // Adjust the values as needed
-  });
-
-  // Use getRandomPosition to set the position of the div
-  const dialogPosition = getRandomPosition();
+  function getRandomPosition() {
+    return {
+      x: Math.floor(Math.random() * 800), // Adjust the values as needed
+      y: Math.floor(Math.random() * 600), // Adjust the values as needed
+    };
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
       {/* Dialog for large screens */}
-      <div 
-        className='hidden md:block'
-        style={{ position: 'absolute', top: `${dialogPosition.y}px`, left: `${dialogPosition.x}px` }}>
-        <DialogBox isOpen={isDialogOpen} onClose={handleCloseDialog} title="2013-2023">
+      {hasRandomPosition && (
+        <div
+          className='hidden md:block'
+          style={{ position: 'absolute', top: `${dialogPosition.y}px`, left: `${dialogPosition.x}px` }}>
+          <DialogBox isOpen={isDialogOpen} onClose={handleCloseDialog} title="2013-2023">
             <p className='mb-5'>After a decade of activity, 2023 will be PC Music&rsquo;s final year of new releases. Following that, the label will be dedicated to archival projects and special reissues. </p>
             <p className='mb-5'>We have an undisclosed number of new albums and singles coming very soon.</p>
             <p className='mb-5'>For now, 10 x 10 minutes of content from the past, present & future.</p>
             <p className='mb-5'>Personal Computer Music forever.</p>
-        </DialogBox>
-      </div>
+          </DialogBox>
+        </div>
+      )}
 
       {/* Dialog for smaller screens */}
-      <div className='md:hidden fixed top-0 left-0 w-full h-full flex justify-center items-center'>
-        <DialogBox isOpen={isDialogOpen} onClose={handleCloseDialog} title="2013-2023">
+      {hasRandomPosition && (
+        <div className='md:hidden fixed top-0 left-0 w-full h-full flex justify-center items-center'>
+          <DialogBox isOpen={isDialogOpen} onClose={handleCloseDialog} title="2013-2023">
             <p className='mb-5'>After a decade of activity, 2023 will be PC Music&rsquo;s final year of new releases. Following that, the label will be dedicated to archival projects and special reissues. </p>
             <p className='mb-5'>We have an undisclosed number of new albums and singles coming very soon.</p>
             <p className='mb-5'>For now, 10 x 10 minutes of content from the past, present & future.</p>
             <p className='mb-5'>Personal Computer Music forever.</p>
-        </DialogBox>
-      </div>
+          </DialogBox>
+        </div>
+      )}
     </main>
   )
 }
